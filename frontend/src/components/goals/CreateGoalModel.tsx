01d526871d33b54856a988916a838901b9274e0b
wramos1 <ramoswillyam1@gmail.com>
2026-02-02 21:05:20 -0800

    Added React boiletplate to prevent errors when running app

---
+++ frontend/src/components/goals/CreateGoalModel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateGoalModel = () => {
+  return <div>CreateGoalModel</div>;
+};
+
+export default CreateGoalModel;
---
+++ frontend/src/components/goals/DepositModel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DepositModel = () => {
+  return <div>DepositModel</div>;
+};
+
+export default DepositModel;
